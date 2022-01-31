33bd6629c30bbf19d1b7168fcda2bc7482650a57
Renou Benteau <abenteau@gmail.com>
2022-01-31 12:05:11 +0100

    remove dotenv loading, now just assume env vars are set

--- sqlalchemy_tools/__init__.py
+++ sqlalchemy_tools/__init__.py
@@ -1,16 +1,14 @@
 import os
-from dotenv import load_dotenv
 import pathlib
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+"""all this stuff assumes that db credentials are set as ENV variables"""
 
-def get_sqlalchemy_url(prefix, env_file=None):
-    if env_file:
-        load_dotenv(env_file)
-    """ build database url from env file """
+def get_sqlalchemy_url(prefix):
+    """ build database url from env vars """
     DB_TYPE = os.environ.get(prefix+'_DB_TYPE')
     DB_USERNAME = os.environ.get(prefix+'_DB_USERNAME')
     DB_PASS = os.environ.get(prefix+'_DB_PASSWORD')
@@ -20,13 +18,11 @@ def get_sqlalchemy_url(prefix, env_file=None):
     sqlalchemy_url = DB_TYPE+"://"+DB_USERNAME+":"+DB_PASS+"@"+DB_HOST+":"+DB_PORT+"/"+DB_NAME
     return sqlalchemy_url
 
-def get_sqlalchemy_scoped_session(env_file=".env", prefix="TEST"):
+def get_sqlalchemy_scoped_session(prefix="TEST"):
     '''
     connect to database and create sqlalchemy session.
-    env_file: path to env file where database URL and credentials are stored
     prefix: string appended to database variable names. ex: PREFIX_DB_PASSWORD
     '''    
-    dotenv = load_dotenv(env_file)
     sqlalchemy_url = get_sqlalchemy_url(prefix)
     engine = create_engine(sqlalchemy_url)
     engine.connect()
@@ -35,18 +31,17 @@ def get_sqlalchemy_scoped_session(env_file=".env", prefix="TEST"):
     return Session()
 
 
-def get_base(env_file=".env", prefix="TEST"):
+def get_base(prefix="TEST"):
     """ get model base, so that the model mappings of the orm can be used in other"""
-    dotenv = load_dotenv(env_file)
     sqlalchemy_url = get_sqlalchemy_url(prefix)
     Base = automap_base()
     engine = sqlalchemy.create_engine(sqlalchemy_url)
     Base.prepare(engine, reflect=True)
     return Base
     
-def empty_tables(env_file=".env", prefix="TEST"):
-    base = get_base(env_file, prefix)
-    session = get_sqlalchemy_scoped_session(env_file, prefix)
+def empty_tables(prefix="TEST"):
+    base = get_base(prefix)
+    session = get_sqlalchemy_scoped_session(prefix)
     for Table in base.classes:
         session.query(Table).delete()
     session.commit()
